a0c091f3b0cecc283f9cd37029ee524f5c444b08
marleenkupri <leenu364@gmail.com>
2026-02-01 18:46:47 +0200

    uudet

---
+++ Ohjelmisto-1-kevät/moduuli 6/muistiinpanot.py
@@ -0,0 +1,10 @@
+def tervehdi(kerta):
+    for i in range(kerta):
+        print(f"Heippa! {i + 1}, kerta")
+    print("Olen funktion lopussa.")
+    return
+
+# tästä alkaa ohjelma pääosuus (main):
+tervehdi(5)
+tervehdi(1)
+
