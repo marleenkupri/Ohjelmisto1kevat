6358f495e2bb89eecdbc60f0f6700e476f338823
marleenkupri <leenu364@gmail.com>
2026-02-02 15:34:35 +0200

    uudet

---
+++ Ohjelmisto-1-kevät/moduuli 7/tehtävä 1.py
@@ -0,0 +1,22 @@
+def get_season(month):
+    if month in (12, 1, 2):
+        return "winter"
+    elif month in (3, 4, 5):
+        return "spring"
+    elif month in (6, 7, 8):
+        return "summer"
+    elif month in (9, 10, 11):
+        return "autumn"
+
+try:
+    month_input = int(input("Enter the number of a month (1-12): "))
+    print(f"You entered: {month_input}")
+
+    if 1 <= month_input <= 12:
+        season = get_season(month_input)
+        print(f"The season is {season}.")
+    else:
+        print("Please enter a number between 1 and 12.")
+
+except ValueError:
+    print("Please enter a number between 1 and 12.")
---
+++ Ohjelmisto-1-kevät/moduuli 7/tehtävä 2.py
@@ -0,0 +1,15 @@
+names = set()
+
+while True:
+    name = input()
+    if name == "":
+        break
+
+    if name in names:
+        print("Existing name")
+    else:
+        print("New name")
+        names.add(name)
+
+for name in names:
+    print(name)
